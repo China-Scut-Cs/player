e892446a254efdb8d2bc68b134675bb4186e6a0d
LPHlph123456 <104425287+LPHlph123456@users.noreply.github.com>
2022-04-26 20:15:46 +0800

    basic function

--- ui_widget.h
+++ ui_widget.h
@@ -6,6 +6,7 @@
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
+
 #ifndef UI_WIDGET_H
 #define UI_WIDGET_H
 
