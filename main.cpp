67be411d0c712cb1e20f6fdc49d144b61c28db1e
LPHlph123456 <104425287+LPHlph123456@users.noreply.github.com>
2022-04-26 20:09:29 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,7 @@
 
 #include <QApplication>
 #include <QDebug>
-extern "C"
-{
-#include <libavcodec/avcodec.h>
-#include <libavdevice/avdevice.h>
-#include <libavfilter/avfilter.h>
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libpostproc/postprocess.h>
-#include <libswscale/swscale.h>
-#include <libswresample/swresample.h>
-}
+
 
 int main(int argc, char *argv[])
 {
