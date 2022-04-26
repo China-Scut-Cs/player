c3f3f9895eb049a6823435121d602c3595b1bb0e
LPHlph123456 <104425287+LPHlph123456@users.noreply.github.com>
2022-04-26 20:10:47 +0800

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -11,17 +11,7 @@
 #include <QMessageBox>
 #include <QDebug>
 #include <QDesktopWidget>
-extern "C"
-{
-#include <libavcodec/avcodec.h>
-#include <libavdevice/avdevice.h>
-#include <libavfilter/avfilter.h>
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libpostproc/postprocess.h>
-#include <libswscale/swscale.h>
-#include <libswresample/swresample.h>
-}
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
