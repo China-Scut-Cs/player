46c5aaaa60f9d5aa00552507e6e1a6de7c6e3ff0
China-Scut-Cs <90946154+China-Scut-Cs@users.noreply.github.com>
2022-05-13 20:51:59 +0800

    Delete src directory

--- src/ffmpeg.cpp
+++
@@ -1,8 +0,0 @@
-#include "Player.h"
-
-int main() {
-    Player *player = new Player();
-    player->playerInit("D:/Qt/Project/VideoPlayer/Player/resources/video/Stray Kids - 말할 수 없는 비밀 (Secret Secret).mkv", 900, 600);
-    player->Play();
-    qDebug()<<"SUCCESS";
-}
